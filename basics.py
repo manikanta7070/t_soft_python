db370270c13b4975af0a496e2253caf5abf27ba9
manikanta7070 <manidaggu7070@gmail.com>
2023-07-25 16:12:17 +0530

    python programming

---
+++ basics.py
@@ -0,0 +1,39 @@
+#Assigning single value to multiple variables
+a=b=c=10
+print(a)
+print(b)
+print(c)
+
+print("--------------------------")
+
+a=b=c=10
+print(a,b,c,sep=",")
+
+print("--------------------------")
+
+#assigning multiple values to multiple variables
+
+a,b,c=10,20,30
+print(a)
+print(b)
+print(c)
+
+
+print("--------------------------")
+
+#syntax to get keyword list
+import keyword
+print(keyword.kwlist)
+
+print("--------------------------")
+
+# Syntax to read the input
+a=input("enter a number :")
+print(a)
+name= input("enter a string : ")
+print(name)
+
+print("--------------------------")
+
+
+
